df6111cf7a4bf832338245a17796c9bdf17036c6
Bill Shi <shirunjie@126.com>
2019-02-02 14:23:49 -0500

    Cross Entropy

--- a1/starter.py
+++ a1/starter.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import time
+from scipy.special import expit
 
 def loadData():
     with np.load('notMNIST.npz') as data :
@@ -51,35 +52,70 @@ def gradMSE(W, b, x, y, reg):
 
 def crossEntropyLoss(W, b, x, y, reg):
     # Your implementation here
-    pass
+    z = W.T.dot(x) + b
+    yhat = expit(z)
+    
+    N = y.size
+    
+    cel = 1.0 / N * np.sum(
+            -y * np.log(yhat) - 
+            -(1-y) * np.log(1-yhat)            
+            ) + reg / 2.0 * W.T.dot(W)
+    
+    return cel
 
 def gradCE(W, b, x, y, reg):
     # Your implementation here
-    pass
+    z = W.T.dot(x) + b
+    yhat = expit(z)
 
-def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS,
+    dyhatdb = yhat * (1-yhat)
+    dyhatdw = dyhatdb * x
+
+    N = y.size
+
+    dldw = 1.0 / N * np.sum(
+        -y * 1.0 / yhat * dyhatdw +
+        (1-y) * 1.0 / (1-yhat) * dyhatdw
+    ) + reg * W
+
+    dldb = 1.0 / N * np.sum(
+        -y * 1.0 / yhat * dyhatdb +
+        (1-y) * 1.0 / (1-yhat) * dyhatdb
+    )
+
+    return dldw, dldb
+
+def grad_descent(W, b, trainingData, trainingLabels, alpha, iterations, reg, EPS, lossType=None,
                  validData=None, validTarget=None, testData=None, testTarget=None):
     # Your implementation here
+    if lossType is None or lossType == 'mse':
+        errorFun = MSE
+        gradErrorFun = gradMSE
+    else:
+        errorFun = crossEntropyLoss
+        gradErrorFun = gradCE
+
     mseList = []
     validMseList = []
     testMseList = []
     for i in range(iterations):
-        if i % 5 == 0:
+        if i % 50 == 49:
             print('\rIteration %03d/%03d' % (i, iterations), end='\r')
-        gradW, gradB= gradMSE(W, b, trainingData, trainingLabels, reg)
+        gradW, gradB = gradErrorFun(W, b, trainingData, trainingLabels, reg)
         W = W - alpha * gradW
         b = b - alpha * gradB
-        mse = MSE(W, b, trainingData, trainingLabels, reg)
+        mse = errorFun(W, b, trainingData, trainingLabels, reg)
         mse = np.asscalar(mse)
         mseList.append(mse)
         
         if validData is not None and validTarget is not None:
             validMseList.append(np.asscalar(
-                    MSE(W, b, validData, validTarget, reg)
+                    errorFun(W, b, validData, validTarget, reg)
                     ))
         if testData is not None and testTarget is not None:
             testMseList.append(np.asscalar(
-                    MSE(W, b, testData, testTarget, reg)
+                    errorFun(W, b, testData, testTarget, reg)
                     ))
         
         if np.abs(mse) < EPS:
@@ -97,12 +133,15 @@ def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rat
 
 
 if __name__ == '__main__':
+    # %%
     linear_regression_epochs = 5000
     run13 = True
     run14 = True
     save_linear_regression = False
     plot_linear_regression = True
-    run_logistic_regression = True
+
+    logistic_regression_epochs = 5000
+    
     run_sgd = True    
     
     trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
@@ -110,84 +149,87 @@ if __name__ == '__main__':
     trainDataVec = trainData.reshape([trainData.shape[0], trainData.shape[1]*trainData.shape[2]]).T
     validDataVec = validData.reshape([validData.shape[0], validData.shape[1]*validData.shape[2]]).T
     testDataVec = testData.reshape([testData.shape[0], testData.shape[1]*testData.shape[2]]).T
-    
-    if linear_regression_epochs:
-        def train(alphaLambdList, figfilename=None):
-            mse = []
-            validMse = []
-            testMse = []
-            weight = []
-            bias = []
-            accu = []
-            runtime = []
-            
+
+
+    def train(alphaLambdList, figfilename=None, lossType=None):
+        mse = []
+        validMse = []
+        testMse = []
+        weight = []
+        bias = []
+        accu = []
+        runtime = []
+
+        for alpha, lambd in alphaLambdList:
+            print('Alpha =', alpha, 'Lambda =', lambd)
+
+            W = np.zeros([trainDataVec.shape[0], 1])
+            b = np.array([[0]])
+            tic = time.clock()
+            W, b, mseList, validMseList, testMseList = grad_descent(
+                W, b, trainDataVec, trainTarget.T, alpha,
+                linear_regression_epochs, lambd, 1e-6, lossType,
+                validDataVec, validTarget.T, testDataVec, testTarget.T)
+            runtime.append(time.clock() - tic)
+
+            mse.append(mseList)
+            validMse.append(validMseList)
+            testMse.append(testMseList)
+
+            weight.append(W)
+            bias.append(b)
+
             def accuracy(x, y):
                 y = y.T
                 return np.count_nonzero((W.T.dot(x) + b > 0.5) == y) * 1.0 / y.size
-                
-            for alpha, lambd in alphaLambdList:
-                print('Alpha =', alpha, 'Lambda =', lambd)
-                
-                W = np.zeros([trainDataVec.shape[0], 1])
-                b = np.array([[0]])
-                tic = time.clock()
-                W, b, mseList, validMseList, testMseList = grad_descent(
-                        W, b, trainDataVec, trainTarget.T, alpha, 
-                        linear_regression_epochs, lambd, 1e-6,
-                        validDataVec, validTarget.T, testDataVec, testTarget.T)
-                runtime.append(time.clock() - tic)
-                
-                mse.append(mseList)
-                validMse.append(validMseList)
-                testMse.append(testMseList)
-                
-                weight.append(W)
-                bias.append(b)
-                
-                accu.append((
-                        accuracy(trainDataVec, trainTarget), 
-                        accuracy(validDataVec, validTarget), 
-                        accuracy(testDataVec, testTarget), 	
-                        ))
-                
-            mse = np.array(mse)
-            validMse = np.array(validMse)
-            testMse = np.array(testMse)
-            
-            weight = np.array(weight)
-            bias = np.array(bias)
-            
-            accu = np.array(accu)
-            
-            if save_linear_regression and figfilename is not None:
-                np.savez(figfilename + '.npz', mse=mse, weight=weight, bias=bias)
-            
-            if plot_linear_regression:
-                fig, axs = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
-                for i, (err, title) in enumerate(zip(
-                        (mse, validMse, testMse),
-                        ('Training Loss', 'Validation Loss', 'Testing Loss'),
-                        )):
-                    axs[i].plot(err.T)
-                    axs[i].set_title(title)
-                    axs[i].grid()
-                    axs[i].set_xlabel('Epoches')
-                    axs[i].set_ylabel('Mean Square Error')
-                
-                axs[i].legend([r'$\alpha=%g,\lambda=%g$' % (alpha, lambd) 
-                                for alpha, lambd in alphaLambdList])
-                if figfilename:
-                    plt.savefig(figfilename, dpi=150)
-                
-                print('Alpha\tLambda\tRuntime\tTraining Error\tValidation Error\tTesting Error\tTraining Accuracy\tValidation Accuracy\tTesting Accuracy')
-                for (alpha, lambd), mseList, validList, testList, accuList, t in zip(
-                        alphaLambdList, mse, validMse, testMse, accu, runtime):
-                    print(alpha, lambd, t, mseList[-1], validList[-1], testList[-1], 
-                          *accuList,
-                          sep='\t')
-        
-        
-        
+
+            accu.append((
+                accuracy(trainDataVec, trainTarget),
+                accuracy(validDataVec, validTarget),
+                accuracy(testDataVec, testTarget),
+            ))
+
+        mse = np.array(mse)
+        validMse = np.array(validMse)
+        testMse = np.array(testMse)
+
+        weight = np.array(weight)
+        bias = np.array(bias)
+
+        accu = np.array(accu)
+
+        if save_linear_regression and figfilename is not None:
+            np.savez(figfilename + '.npz', mse=mse, weight=weight, bias=bias)
+
+        if plot_linear_regression:
+            fig, axs = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
+            for i, (err, title) in enumerate(zip(
+                    (mse, validMse, testMse),
+                    ('Training Loss', 'Validation Loss', 'Testing Loss'),
+            )):
+                axs[i].plot(err.T)
+                axs[i].set_title(title)
+                axs[i].grid()
+                axs[i].set_xlabel('Epoches')
+                axs[i].set_ylabel('Mean Square Error')
+
+            axs[-1].legend([r'$\alpha=%g,\lambda=%g$' % (alpha, lambd)
+                            for alpha, lambd in alphaLambdList])
+            if figfilename:
+                plt.savefig(figfilename, dpi=150)
+
+            print('Alpha', 'Lambda', 'Runtime',
+                  'Training Error', 'Validation Error', 'Testing Error',
+                  'Training Accuracy', 'Validation Accuracy', 'Testing Accuracy', sep='\t')
+            for (alpha, lambd), mseList, validList, testList, accuList, t in zip(
+                    alphaLambdList, mse, validMse, testMse, accu, runtime):
+                print(alpha, lambd, t,
+                      mseList[-1], validList[-1], testList[-1],
+                      *accuList,
+                      sep='\t')
+
+    # %% Q1
+    if linear_regression_epochs:
         #%% Part 1.3
         if run13:
             train([(0.005, 0), (0.001, 0), (0.0001, 0)], figfilename='fig13.png')
@@ -213,8 +255,10 @@ if __name__ == '__main__':
         print('%.3f' % accuracy(trainDataVec, trainTarget), '\t', 
               '%.3f' % accuracy(validDataVec, validTarget), '\t', 
               '%.3f' % accuracy(testDataVec, testTarget))
-        
-        
+
+    # %% Q2
+    if logistic_regression_epochs:
+        pass
     
     
     
