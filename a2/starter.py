6988b49dd08dc926bfbb6450f48f1dcc7a20bc44
Bill Shi <shirunjie@126.com>
2019-03-09 14:57:02 -0500

    helper function

--- a2/starter.py
+++ a2/starter.py
@@ -44,20 +44,99 @@ def shuffle(trainData, trainTarget):
 
 
 def relu(x):
-    # TODO
+    """
+    >>> %timeit np.where(x>0, x, 0)
+    145 µs ± 12.9 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
+    >>> %timeit x * (x>0)
+    190 µs ± 2.78 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+    >>> %timeit np.maximum(x, 0)
+    983 µs ± 5.81 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+
+    :param x:
+    :return:
+    """
+    return np.where(x > 0, x, 0)
 
 def softmax(x):
-    # TODO
+    """
 
+    :param x:
+    :return:
+    """
+    ex = np.exp(x)
+    return ex / np.sum(ex)
 
 def computeLayer(X, W, b):
-    # TODO
+    """
+
+    :param X: input, m x 1
+    :param W: weight, m x n
+    :param b: bias, n x 1
+    :return:
+    """
+    X = np.asarray(X).reshape((-1, 1))
+    W = np.atleast_2d(W)
+    b = np.asarray(b).reshape((-1, 1))
+
+    m = X.size
+    n = W.shape[1]
+    assert X.shape == (m, 1)
+    assert W.shape[0] == m
+
+    ret = X.T.dot(W)
+
+    ret = ret.T
+    assert ret.shape == (n, 1)
+
+    ret += b
+    assert ret.shape == (n, 1)
+
+    return ret
 
 def CE(target, prediction):
+    """
 
-    # TODO
+    :param target:
+    :param prediction:
+    :return:
+    """
+    t = np.atleast_2d(target)
+    s = np.atleast_2d(prediction)
+    assert t.shape == s.shape
+
+    N, K = t.shape
+    assert K == 10
+
+    logs = np.log(s)
+    tlogs = t * logs
+
+    averagece = - 1.0 / N * np.sum(tlogs)
+
+    return float(averagece)
 
 def gradCE(target, prediction):
+    """
 
-    # TODO
+    :param target:
+    :param prediction:
+    :return:
+    """
+    t = np.atleast_2d(target)
+    s = np.atleast_2d(prediction)
+    assert t.shape == s.shape
 
+    N, K = t.shape
+    assert K == 10
+
+    ret = - 1.0 / N * t / s
+    assert ret.shape == t.shape
+
+    return ret
+
+
+if __name__ == '__main__':
+    trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+    newtrain, newvalid, newtest = convertOneHot(trainTarget, validTarget, testTarget)
+
+    def q1():
+        pass
