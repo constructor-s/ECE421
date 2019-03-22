dced2d8aac0d95614f6e466b3766421b54709b09
winnie <winnieyannn@hotmail.com>
2019-03-22 15:35:03 -0400

    a3

---
+++ a3/starter_kmeans.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import helper as hlp
+
+# Loading data
+data = np.load('data2D.npy')
+print(data.shape)
+#data = np.load('data100D.npy')
+[num_pts, dim] = np.shape(data)
+
+#v1 = data[:,0]
+#v2 = data[:,1]
+#
+#x = np.array(list(zip(v1,v2)))
+#plt.scatter(v1,v2)
+
+
+# For Validation set
+if is_valid:
+  valid_batch = int(num_pts / 3.0)
+  np.random.seed(45689)
+  rnd_idx = np.arange(num_pts)
+  np.random.shuffle(rnd_idx)
+  val_data = data[rnd_idx[:valid_batch]]
+  data = data[rnd_idx[valid_batch:]]
+
+# Distance function for K-means
+def distanceFunc(X, MU):
+    # Inputs
+    # X: is an NxD matrix (N observations and D dimensions)
+    # MU: is an KxD matrix (K means and D dimensions)
+    # Outputs
+    # pair_dist: is the pairwise distance matrix (NxK)
+    # TODO
+    N = X.shape[0]
+    K = MU.shape[0]
+    D = X.shape[1]
+
+    X = X.reshape(N,1,D)
+    pair_dist = zeros((N,K))
+    
+    diff = X - MU
+    sq_sum = np.sum(diff ** 2, axis = 2)
+    
+    pair_dist = np.sqrt(sq_sum)
+    
+    return pair_dist
