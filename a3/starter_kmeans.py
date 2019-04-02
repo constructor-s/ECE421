fa07d8a5e1a0cc93fd162875fccbfa0ba0cfc12f
Bill Shi <shirunjie@126.com>
2019-04-02 13:02:49 -0400

    1.1

--- a3/starter_kmeans.py
+++ a3/starter_kmeans.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
-from scipy.stats import norm
 
 # Loading data
 data = np.load('data2D.npy')
@@ -10,22 +9,15 @@ data = np.load('data2D.npy')
 #data = np.load('data100D.npy')
 [num_pts, dim] = np.shape(data)
 
-##ploting data
-#v1 = data[:,0]
-#v2 = data[:,1]
-#
-#x = np.array(list(zip(v1,v2)))
-#plt.scatter(v1,v2)
-
 
 # For Validation set
-#if is_valid:
-#  valid_batch = int(num_pts / 3.0)
-#  np.random.seed(45689)
-#  rnd_idx = np.arange(num_pts)
-#  np.random.shuffle(rnd_idx)
-#  val_data = data[rnd_idx[:valid_batch]]
-#  data = data[rnd_idx[valid_batch:]]
+if is_valid:
+  valid_batch = int(num_pts / 3.0)
+  np.random.seed(45689)
+  rnd_idx = np.arange(num_pts)
+  np.random.shuffle(rnd_idx)
+  val_data = data[rnd_idx[:valid_batch]]
+  data = data[rnd_idx[valid_batch:]]
 
 # Distance function for K-means
 def distanceFunc(X, MU):
@@ -34,36 +26,26 @@ def distanceFunc(X, MU):
     # MU: is an KxD matrix (K means and D dimensions)
     # Outputs
     # pair_dist: is the pairwise distance matrix (NxK)
-    # TODO
     
-    with tf.variable_scope('distanceFunc'):
-        x_sq = tf.reduce_sum(tf.square(X), 1)
-        mu_sq = tf.reduce_sum(tf.square(MU), 1)
-        
-        print(x_sq)
-        
-        x_sq = tf.reshape(x_sq, [-1,1])
-        mu_sq = tf.reshape(mu_sq, [1,-1])
-        
-        distance = tf.sqrt(tf.maximum(x_sq - 2 * tf.matmul(X, MU, False, True) + mu_sq, 0.0))
-        
-        print(distance)
-    return distance
+    if isinstance(X, np.ndarray):
+        reduce_sum = np.sum
+        reshape = np.reshape
+    elif isinstance(X, tf.Tensor):
+        reduce_sum = tf.reduce_sum
+        reshape = tf.reshape
     
+    N = X.shape[0]
+    K = MU.shape[0]
+    D = X.shape[1]
+
+    X = reshape(X, (N,1,D))
     
-#    N = X.shape[0]
-#    K = MU.shape[0]
-#    D = X.shape[1]
-#
-#    X = X.reshape(N,1,D)
-#    pair_dist = np.zeros((N,K))
-#    
-#    diff = X - MU
-#    sq_sum = np.sum(diff ** 2, axis = 2)
-#    
-#    pair_dist = np.sqrt(sq_sum)
-#    
-#    return pair_dist
+    diff = X - MU
+    sq_sum = reduce_sum(diff ** 2, axis = 2)
+    
+    assert sq_sum.shape == (N, K)
+    
+    return sq_sum
     
 def assign(data, k, learn_rate=0.01):
     
@@ -113,7 +95,63 @@ def update(data, k, learn_rate=0.01):
     
     
 if __name__ == '__main__':
-    k = 3
-    update(data, k, 0.01)
+    tf.reset_default_graph()
     
+    K = 3
+    D = data.shape[1]
+    N = data.shape[0]
     
+    X = tf.placeholder(dtype=tf.float32, shape=(N, D), name='X')
+    
+    MU = tf.get_variable(name='MU', shape=(K, D), dtype=tf.float32, 
+                         initializer=tf.initializers.random_normal,
+                         trainable=True)
+    
+    sq_dist = distanceFunc(X, MU)
+    
+    pred = tf.argmin(sq_dist, axis=1)
+    min_sq_dist = tf.reduce_min(sq_dist, axis=1)
+    
+    loss = tf.reduce_mean(min_sq_dist, axis=0)
+    
+    assert loss.shape == ()
+    
+    optim = tf.train.AdamOptimizer(learning_rate=1e-2,
+                                    beta1=0.9,
+                                    beta2=0.99,
+                                    epsilon=1e-5)
+    train_op = optim.minimize(loss)
+    
+    # Add an op to initialize the variables.
+    init_op = tf.global_variables_initializer()
+    epoches = 500
+    loss_curve = []
+    pred_list = []
+    with tf.Session() as sess:
+        # Run the init operation.
+        sess.run(init_op)
+        
+        for i in range(epoches):
+            _, loss_val, pred_val = sess.run([train_op, loss, pred], feed_dict={X: data})
+            loss_curve.append(loss_val)
+            pred_list.append(pred_val)
+#            if len(pred_list) >= 2 and np.all(pred_list[-1] == pred_list[-2]):
+#                print('Converged after %d iterations' % i)
+#                break
+        
+        
+    fig, ax = plt.subplots()
+    ax.plot(loss_curve)
+    ax.set_xlabel('Number of updates')
+    ax.set_ylabel('Loss')
+    ax.grid()
+    fig.savefig('1.1.png')
+    fig.show()
+        
+        
+        
+        
+        
+        
+        
+        
